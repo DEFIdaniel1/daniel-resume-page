b26b99b4ff891f6893249def677860cf9ac44859
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-05-10 16:20:57 -0600

    UI tweaks and rebuild/redeploy

--- src/Components/Layout/HeroImage.js
+++ src/Components/Layout/HeroImage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HeroImage.styles.scss";
-import { Row, Col } from "react-bootstrap";
+// import { Row, Col } from "react-bootstrap";
 
 const IntroImage = () => {
   return (
@@ -29,27 +29,12 @@ const IntroImage = () => {
         <span></span>
         <span></span>
         <span></span>
-        <Row className="backgroundImgDiv">
-          <Col md={3} sm={2} xs={1}>
-            <div>
-              <h2 className="bracket-top">
-                <i class="fa-solid fa-chevron-left fa-2x"></i>
-              </h2>
-            </div>
-          </Col>
-          <Col md={6} xs={10}>
-            <h1 className="fade-in1">I'm Daniel...</h1>
-            {/* <div>
-              <Image src={laptop} alt="application icon" width="60%" />
-            </div> */}
-            <h2 className="fade-in2">...a blockchain and web app developer!</h2>
-          </Col>
+        <section className="hero-text">
           <div>
-            <h2 className="bracket-bottom">
-              <i class="fa-solid fa-chevron-right fa-2x"></i>
-            </h2>
+            <h1 className="fade-in1">I'm Daniel...</h1>
+            <h2 className="fade-in2">...a blockchain and web app developer!</h2>
           </div>
-        </Row>
+        </section>
       </div>
     </>
   );
--- src/Components/UI/TransitionPage.js
+++ src/Components/UI/TransitionPage.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import './TransitionPage.scss';
+import React from "react";
+import "./TransitionPage.scss";
 
 const TransitionPage = (props) => {
   return (
     <div class="hover-div">
       <div className="div2 fade">
         <h1 className="hover-el">{props.title}</h1>
-      </div>
-      <div>
         <h2 className="hover-el fade">
-          <i class="fa-solid fa-angles-down icon"></i>
+          <i class="fa-solid fa-angles-down"></i>
         </h2>
       </div>
     </div>
   );
-}
+};
 
-export default TransitionPage
+export default TransitionPage;
